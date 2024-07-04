1ec5ff7d0a6d795de8b30204a494da48c06f41c3
useyournose <mroliverberger@gmx.de>
2024-07-04 23:04:45 +0200

    first test

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,4 @@
-//import { Decoder, Stream, Profile, Utils } from '@garmin/fitsdk';
-//import fs from 'fs';
-//import path from 'path';
+import { Stream, Decoder } from "https://cdn.jsdelivr.net/npm/@garmin/fitsdk@21.141.0/src/index.min.js";
 
 let inputElement = document.getElementById('fit2labradar');
 inputElement.addEventListener("change", handleFiles, false);
@@ -10,18 +8,28 @@ function handleFiles() {
   let fileReader = new FileReader();
 
   fileReader.onload = function(event) {
-    let fitFileData = event.target.result;
-    fit2labradar(fitFileData);
+      let fitFileData = event.target.result;
+      fit2labradar(fitFileData);
   };
-  
+
   fileReader.readAsArrayBuffer(fileList[0]);
 }
 
-function convertToJSON(fileData) {
-  let formattedData = fileData.replace(/\r?\n/g, '\\n').replace(/\t/g, '\\t');
-  let jsonData = '[' + formattedData + ']';
-  
-  document.getElementById('output').innerHTML = jsonData;
+function download(text) {
+  var fullPath = document.getElementById('fit2labradar').value;
+  filename = fullPath.split(/(\\|\/)/g).pop()
+  filename = filename.replace(/\.fit$/, '.csv');
+
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+  document.getElementById('fit2labradar').value = "";
+  alert("file saved");
 }
 
 function get_ke(velocity_in_ms, weight_in_grains) {
@@ -30,63 +38,45 @@ function get_ke(velocity_in_ms, weight_in_grains) {
 }
 
 function fit2labradar(fileData) {
-    //const inputfile = './in/06-18-2024_15-47-11.fit'
-    //const outputfile = './out/out.csv'
+  const streamfromFileSync = Stream.fromArrayBuffer(fileData);
+  const decoder = new Decoder(streamfromFileSync);
+  console.log("isFIT (instance method): " + decoder.isFIT());
+  console.log("checkIntegrity: " + decoder.checkIntegrity());
+  const { messages, errors } = decoder.read({
+      //mesgListener: (messageNumber, message) => {},
+      //applyScaleAndOffset: true,
+      expandSubFields: true,
+      expandComponents: true,
+      convertTypesToStrings: true,
+      convertDateTimesToDates: true,
+      includeUnknownData: true,
+      //mergeHeartRates: true
+  });
+  const DeviceData = messages.deviceInfoMesgs[0]
+  const SessionData = messages.chronoShotSessionMesgs[0]
+  var stream = ""
+  stream+="sep=;\n";
+  stream+="Device ID;" + DeviceData.manufacturer +'-'+ DeviceData.serialNumber.toString() + ";;\n\n";
+  stream+="Series No;0001;;\n";
+  stream+="Total number of shots;" +SessionData.shotCount.toString().padStart(4, '0') + ";;\n\n";
 
-    //const buf = fs.readFileSync(inputfile);
-    const streamfromFileSync = Stream.fromArrayBuffer(fileData);
-    const decoder = new Decoder(streamfromFileSync);
+  stream+="Units velocity;m/s;;\n";
+  stream+="Units distances;m;;\n";
+  stream+="Units kinetic energy;j;;\n";
+  stream+="Units weight;grains (grs);;\n\n";
 
-    console.log("isFIT (instance method): " + decoder.isFIT());
-    console.log("checkIntegrity: " + decoder.checkIntegrity());
+  stream+="Stats - Average;"+ SessionData.avgSpeed +";m/s;\n";
+  stream+="Stats - Highest;"+ SessionData.maxSpeed +";m/s;\n";
+  stream+="Stats - Lowest;"+ SessionData.minSpeed +";m/s;\n";
+  stream+="Stats - Ext. Spread;;m/s;\n";
+  stream+="Stats - Std. Dev;;m/s;\n\n";
 
-    const { messages, errors } = decoder.read({
-        //mesgListener: (messageNumber, message) => {},
-        //applyScaleAndOffset: true,
-        expandSubFields: true,
-        expandComponents: true,
-        convertTypesToStrings: true,
-        convertDateTimesToDates: true,
-        includeUnknownData: true,
-        //mergeHeartRates: true
-    });
-
-    console.log(errors);
-    console.log(messages);
-    console.log(messages.fileIdMesgs[0]);
-    console.log(messages.fileCreatorMesgs[0]);
-    console.log(messages.deviceInfoMesgs[0]);
-    console.log(messages.chronoShotSessionMesgs[0]);
-    console.log(messages.chronoShotDataMesgs[0]);
-
-    const DeviceData = messages.deviceInfoMesgs[0]
-    const SessionData = messages.chronoShotSessionMesgs[0]
-
-    var stream = ""//fs.createWriteStream(outputfile, {flags:'a'});
-
-    stream+="sep=;\n";
-    stream+="Device ID;" + DeviceData.manufacturer +'-'+ DeviceData.serialNumber.toString() + ";;\n\n";
-    stream+="Series No;"+ path.basename(inputfile) +";;\n";
-    /*stream.write("Total number of shots;" +SessionData.shotCount.toString().padStart(4, '0') + ";;\n\n");
-
-    stream.write("Units velocity;m/s;;\n");
-    stream.write("Units distances;m;;\n");
-    stream.write("Units kinetic energy;j;;\n");
-    stream.write("Units weight;grains (grs);;\n\n");
-
-    stream.write("Stats - Average;"+ SessionData.avgSpeed +";m/s;\n");
-    stream.write("Stats - Highest;"+ SessionData.maxSpeed +";m/s;\n");
-    stream.write("Stats - Lowest;"+ SessionData.minSpeed +";m/s;\n");
-    stream.write("Stats - Ext. Spread;;m/s;\n");
-    stream.write("Stats - Std. Dev;;m/s;\n\n");
-
-    stream.write("Shot ID;V0;Ke0;Proj. Weight;Date;Time\n")
-    messages.chronoShotDataMesgs.forEach(function (item,index) {
-        const datestring = item.timestamp.getDate() + "-" + (item.timestamp.getMonth()+1) + "-" + item.timestamp.getFullYear() 
-        const timestring = item.timestamp.getHours().toString().padStart(2,'0') + ":" + item.timestamp.getMinutes().toString().padStart(2,'0')+ ":" + item.timestamp.getSeconds().toString().padStart(2,'0')
-        stream.write(item.shotNum.toString().padStart(4, '0') + ";" + item.shotSpeed +";" + get_ke(item.shotSpeed,SessionData.grainWeight) + ";"+ SessionData.grainWeight + ";" + datestring +";" + timestring  + ";\n");
-    })
-
-    stream.end();*/
-    document.getElementById('output').innerHTML = stream
+  stream+="Shot ID;V0;Ke0;Proj. Weight;Date;Time\n";
+  messages.chronoShotDataMesgs.forEach(function (item,index) {
+      const datestring = item.timestamp.getDate().toString().padStart(2,'0') + "-" + (item.timestamp.getMonth()+1).toString().padStart(2,'0') + "-" + item.timestamp.getFullYear() 
+      const timestring = item.timestamp.getHours().toString().padStart(2,'0') + ":" + item.timestamp.getMinutes().toString().padStart(2,'0')+ ":" + item.timestamp.getSeconds().toString().padStart(2,'0')
+      stream+=item.shotNum.toString().padStart(4, '0') + ";" + item.shotSpeed +";" + get_ke(item.shotSpeed,SessionData.grainWeight) + ";"+ SessionData.grainWeight + ";" + datestring +";" + timestring  + ";\n";
+  })
+  
+  download(stream)
 }
