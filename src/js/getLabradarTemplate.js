e24eca607f8b3e4fde5140a4a566647d30902e37
useyournose <mroliverberger@gmx.de>
2024-07-17 00:44:22 +0200

    fixed getlabradar test

--- src/js/getLabradarTemplate.js
+++ src/js/getLabradarTemplate.js
@@ -13,23 +13,23 @@ function getLabradartemplate() {
     {SPEED_SD}
     */
     return `sep=;
-  Device ID;{DEVICEID};;
-  
-  Series No;0001;;
-  Total number of shots;{SHOTS_TOTAL};;
-  
-  Units velocity;{UNIT_VELOCITY};;
-  Units distances;{UNIT_DISTANCE};;
-  Units kinetic energy;{UNIT_ENERGY};;
-  Units weight;{UNIT_WEIGHT};;
-  
-  Stats - Average;{SPEED_AVG};{UNIT_VELOCITY};
-  Stats - Highest;{SPEED_MAX};{UNIT_VELOCITY};
-  Stats - Lowest;{SPEED_MIN};{UNIT_VELOCITY};
-  Stats - Ext. Spread;{SPEED_ES};{UNIT_VELOCITY};
-  Stats - Std. Dev;{SPEED_SD};{UNIT_VELOCITY};
-  
-  Shot ID;V0;Ke0;Proj. Weight;Date;Time\n`
+Device ID;{DEVICEID};;
+
+Series No;0001;;
+Total number of shots;{SHOTS_TOTAL};;
+
+Units velocity;{UNIT_VELOCITY};;
+Units distances;{UNIT_DISTANCE};;
+Units kinetic energy;{UNIT_ENERGY};;
+Units weight;{UNIT_WEIGHT};;
+
+Stats - Average;{SPEED_AVG};{UNIT_VELOCITY};
+Stats - Highest;{SPEED_MAX};{UNIT_VELOCITY};
+Stats - Lowest;{SPEED_MIN};{UNIT_VELOCITY};
+Stats - Ext. Spread;{SPEED_ES};{UNIT_VELOCITY};
+Stats - Std. Dev;{SPEED_SD};{UNIT_VELOCITY};
+
+Shot ID;V0;Ke0;Proj. Weight;Date;Time\n`
   }
 
   module.exports = getLabradartemplate;
