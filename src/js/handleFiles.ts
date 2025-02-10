13094de639f81cb440bb35c463c4d5575ba712c3
useyournose <mroliverberger@gmx.de>
2025-02-10 22:57:23 +0100

    clean first line for filenames

--- src/js/handleFiles.ts
+++ src/js/handleFiles.ts
@@ -1,6 +1,7 @@
 import fit2labradar from "./fit2labradar";
 import csv2labradar from "./csv2labradar";
-import xls2labradar from "./xls2labradar";
+//import xls2labradar from "./xls2labradar";
+import xls2labradar from "./xls2labradar_v2";
 import { showError } from "./messages";
 
 export default function handleFiles() {
@@ -25,7 +26,8 @@ export default function handleFiles() {
           const result_c:Promise<string> = csv2labradar(FileData, filename);
           result_c.then((value) => {console.log("[handlefiles]: success")},(error) => {showError(error)});
         } else if (sourceid == 'xls2labradar') {
-          await xls2labradar(FileData, filename);
+          const result_x:Promise<string> = xls2labradar(FileData, filename);
+          result_x.then((value) => {console.log("[handlefiles]: success")},(error) => {showError(error)});
         } else {
           console.error('[handlefiles]: what is ' + sourceid + ' doing here?');
         }
--- src/js/xls2labradar.ts
+++ src/js/xls2labradar.ts
@@ -3,16 +3,16 @@ import csv2labradar from "./csv2labradar";
 import { showError } from "./messages";
 
 export default async function xls2labradar(fileData:ArrayBuffer,ofilename:string) {
-    const xlsfile = XLSX.read(fileData, {type:"array"});
-    for (const sheetname of xlsfile.SheetNames) {
-      const worksheet = xlsfile.Sheets[sheetname]
-      const csvdata = XLSX.utils.sheet_to_csv(worksheet);
-      //const title = csvdata.split('\n')[0].replace(/,/g,'').replace(/"/g,'');
-      const title = sheetname;
-      const result:Promise<string> = csv2labradar(csvdata,title+'.csv')
-      result
-      .then((value) => {console.log("[xls2labradar]: "+value)})
-      .catch((value) => {console.warn("[xls2labradar]: "+value); showError(value)})
-      console.log("[xls2labradar]: parsed "+sheetname);
-    }
+  const xlsfile = XLSX.read(fileData, {type:"array"});
+  for (const sheetname of xlsfile.SheetNames) {
+    const worksheet = xlsfile.Sheets[sheetname]
+    const csvdata = XLSX.utils.sheet_to_csv(worksheet);
+    const title = csvdata.split('\n')[0].replace(/,{2,}/g,'').replace(/"/g,'');
+    //const title = sheetname;
+    const result:Promise<string> = csv2labradar(csvdata,title+'.csv')
+    result
+    .then((value) => {console.log("[xls2labradar]: "+value)})
+    .catch((value) => {console.warn("[xls2labradar]: "+value); showError(value)})
+    console.log("[xls2labradar]: parsed "+sheetname);
+  }
 }
